1dceaf9e5e0e0176a641c7fd91987d50fdafe1e2
rosa1435 <158241447+rosa1435@users.noreply.github.com>
2024-04-01 09:57:48 +0900

    FEAT: 토큰을 이용하여 유저 정보 받아오기

--- src/routes/users/user.controller.js
+++ src/routes/users/user.controller.js
@@ -1,68 +1,41 @@
-// // import {userService} from './user.service.js'
-// import axios from 'axios';
-
-// export const kakaoLoginController = async (req, res) => {
-//     const ID = process.env.KAKAO_REST_API_KEY;
-//     const redirect = process.env.KAKAO_REDIRECT_URI;
-//     const CODE = req.body.code;
-
-//     const getToken = () => {
-//         try {
-//             axios
-//                 .post(
-//                     `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${ID}&redirect_uri=${redirect}&code=${CODE}`,
-//                     {},
-//                     {
-//                         headers: {
-//                             'Content-type': 'application/x-www-form-urlencoded',
-//                         },
-//                     },
-//                 )
-//                 .then((res) => {
-//                     console.log(res.data.access_token);
-//                     token = res.data.access_token;
-//                 });
-//         } catch (error) {
-//             console.error(error)
-//             return res.status(405).json({ message: '카카오 인증인가코드 오류' });
-//         }
-//     };
-
-//     getToken();
-
-//     console.log(token);
-//     return res.status(200).json({token});
-// };
+import axios from 'axios';
 import axios from 'axios';
 
 export const kakaoLoginController = async (req, res) => {
-    const ID = process.env.KAKAO_REST_API_KEY;
-    const redirect = process.env.KAKAO_REDIRECT_URI;
-    const CODE = req.body.code;
-
-    const getToken = async () => {
-        try {
-            const response = await axios.post(
-                `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${ID}&redirect_uri=${redirect}&code=${CODE}`,
-                {},
-                {
-                    headers: {
-                        'Content-type': 'application/x-www-form-urlencoded',
-                    },
-                },
-            );
-            return response.data.access_token;
-        } catch (error) {
-            console.error(error)
-            throw new Error('카카오 인증인가코드 오류');
-        }
-    };
-
     try {
-        const token = await getToken();
-        console.log(token);
-        return res.status(200).json({ token });
+        const ID = process.env.KAKAO_REST_API_KEY;
+        const redirect = process.env.KAKAO_REDIRECT_URI;
+        const CODE = req.body.code;
+
+        const response = await axios.post(
+            `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${ID}&redirect_uri=${redirect}&code=${CODE}`,
+            {},
+            {
+                headers: {
+                    'Content-type': 'application/x-www-form-urlencoded',
+                },
+            },
+        );
+
+        const accessToken = response.data.access_token;
+
+        const userInfoResponse = await axios.get(
+            'https://kapi.kakao.com/v2/user/me',
+            {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            }
+        );
+
+        const userInfo = userInfoResponse.data;
+
+        console.log(accessToken);
+        console.log(userInfo);
+
+        return res.status(200).json({ accessToken, userInfo });
     } catch (error) {
-        return res.status(405).json({ message: error.message });
+        console.error(error);
+        return res.status(405).json({ message: '카카오 인증 및 사용자 정보 가져오기 오류' });
     }
 };
