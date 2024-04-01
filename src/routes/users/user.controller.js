a5fdb75e51f6d83d46566c440bcc3002b1feccd6
rosa1435 <158241447+rosa1435@users.noreply.github.com>
2024-04-01 21:21:01 +0900

    FEAT: 엑세스토큰 리프레쉬토큰 발급

--- src/routes/users/user.controller.js
+++ src/routes/users/user.controller.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { prisma } from '../../utils/prisma/index.js';
+import jwt from 'jsonwebtoken';
 
 
 export const kakaoLoginController = async (req, res) => {
@@ -18,53 +19,68 @@ export const kakaoLoginController = async (req, res) => {
             },
         );
 
-        const accessToken = response.data.access_token;
+        const kakaoToken = response.data.access_token;
 
-        const userInfoResponse = await axios.get(
-            'https://kapi.kakao.com/v2/user/me',
-            {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`
-                }
-            }
-        );
+        const userInfoResponse = await axios.get('https://kapi.kakao.com/v2/user/me', {
+            headers: {
+                Authorization: `Bearer ${kakaoToken}`,
+            },
+        });
 
         const userInfo = userInfoResponse.data;
 
-        // const { id, kakao_account: { email, profile: { nickname } } } = userInfoResponse.data;
+        const {
+            id,
+            kakao_account: {
+                email,
+                profile: { nickname },
+            },
+        } = userInfoResponse.data;
 
-        // const user = {
-        //     id,
-        //     email,
-        //     nickname
-        // };
+        const user = {
+            id,
+            email,
+            nickname,
+        };
 
-        // const findUser = await prisma.users.findFirst({
-        //     where: {userCheckId : id}
-        // })
+        const findUser = await prisma.users.findFirst({
+            where: { userCheckId: user.id.toString() },
+        });
 
-        // if (!findUser){
-        //     const createUser = await prisma.users.create({
-        //         data: {
-        //             userCheckId: user.id,
-        //             nickname: user.nickname,
-        //             email: user.email,
-        //         }
-        //     })
-        //     return res.status(200).json({ accessToken})
-        // }
+        if (!findUser) {
+            const createUser = await prisma.users.create({
+                data: {
+                    userCheckId: user.id.toString(),
+                    nickname: user.nickname,
+                    email: user.email,
+                },
+            });
 
+            const accessToken = jwt.sign({ userId: createUser.userId }, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: process.env.ACCESS_TOKEN_LIFE,
+            });
+            const refreshToken = jwt.sign({},process.env.REFRESH_TOKEN_SECRET, {
+                expiresIn: process.env.REFRESH_TOKEN_LIFE,
+            });
+            return res.status(200).json({ accessToken: `Bearer ${accessToken}`, refreshToken: `Bearer ${refreshToken} `});
+            //return res.status(200).json(userInfo);
+        }
 
-
-
-
+        const accessToken = jwt.sign({ userId: findUser.userId }, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: process.env.ACCESS_TOKEN_LIFE,
+        });
+        const refreshToken = jwt.sign({},process.env.REFRESH_TOKEN_SECRET, { expiresIn: process.env.REFRESH_TOKEN_LIFE });
 
         console.log(accessToken);
-        console.log(userInfo);
-        //console.log(findUser);
-        //console.log(createUser);
-         return res.status(200).json({ accessToken, userInfo });// 유저정보 포함
-        //return res.status(200).json({ accessToken}); // 유저정보 제외
+        console.log(refreshToken);
+        console.log(createUser.data);
+        console.log(findUser.data);
+        console.log('손흥민 봉준호 페이커 BTS');
+        console.log(createUser);
+        console.log(findUser);
+
+        return res.status(200).json({ accessToken: `Bearer ${accessToken}`, refreshToken: `Bearer ${refreshToken} `});
+        //return res.status(200).json(ID);
     } catch (error) {
         console.error(error);
         return res.status(405).json({ message: '카카오 인증 및 사용자 정보 가져오기 오류' });
