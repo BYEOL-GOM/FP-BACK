7d9a6648a1233d8c91393cec319de2995f099445
friend-0228 <kwj2712@gamil.com>
2024-04-04 19:26:42 +0900

    REFACTOR : userId를 locals에 가져오는걸로 변경

--- src/routes/likes/like.router.js
+++ src/routes/likes/like.router.js
@@ -7,25 +7,29 @@ import {
     getHelpedSolveWorryDetails,
     getTopLikedCommentAuthors,
 } from './like.controller.js';
+import authMiddleware from '../../middlewares/authMiddleware.js';
 
 let router = express.Router({ mergeParams: true });
 
 // 선물 보내기
-router.post('/worries/:worryId/comments/:commentId/sendLike', sendLike);
+router.post('/worries/:worryId/comments/:commentId/sendLike', authMiddleware, sendLike);
 
 // '나의 해결된 고민' 목록 전체 조회
-router.get('/mySolvedWorry/:userId', getSolvedWorries);
+router.get('/mySolvedWorry', authMiddleware, getSolvedWorries);
 
 // '나의 해결된 고민' 상세 조회
-router.get('/mySolvedWorry/:worryId', getSolvedWorryDetails);
+router.get('/mySolvedWorry/:worryId', authMiddleware, getSolvedWorryDetails);
 
 // '내가 해결한 고민' 목록 전체 조회
-router.get('/myHelpedSolvedWorry/:userId', getHelpedSolveWorries);
+router.get('/myHelpedSolvedWorry', authMiddleware, getHelpedSolveWorries);
 
 // '내가 해결한 고민' 상세 조회
-router.get('/myHelpedSolvedWorry/:worryId', getHelpedSolveWorryDetails);
+router.get('/myHelpedSolvedWorry/:worryId', authMiddleware, getHelpedSolveWorryDetails);
 
 // 좋아요를 가장 많이 받은 탑 5위 댓글 조회
 router.get('/top-likes', getTopLikedCommentAuthors);
+
+// 메인 전체 조회
+router.get('/main', authMiddleware, getMain);
 
 export default router;
