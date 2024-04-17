339e481b0474e0fe1370363cfd8b620dbe828553
LetyPark <sunghee.leticia@gmail.com>
2024-04-17 23:41:48 +0900

    배포용 포트번호 .env 추가

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-// import kakaoStrategy from './routes/passport/kakaoStrategy.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import LogMiddleware from './middlewares/logMiddleware.js';
-//import TestLogMiddleware from './middlewares/testLogMiddleware.js';
 import generalErrorHandler from './middlewares/generalErrorMiddleware.js';
 import router from './routes/index.js';
-import passport from 'passport';
-// import session from 'express-session'; // JWT 사용으로 주석 처리
 import jwt from 'jsonwebtoken';
 import bodyParser from 'body-parser';
 import { loadBannedWords } from './utils/bannedWordsLoader.js';
@@ -16,14 +12,13 @@ import { swaggerUi, specs } from './swagger/swaggerOptions.js';
 import './scheduler.js';
 
 const app = express();
-const PORT = 3000; // 환경 변수에서 포트를 설정할 수 있도록 변경
+const PORT = process.env.PORT || 3001; // 개발환경에서는 3000, 배포는 3001
 
 const corsOrigin = process.env.CORS_ORIGIN || '*';
 
 app.use(
     cors({
         origin: corsOrigin,
-
         methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
         allowedHeaders: ['Content-Type', 'Authorization'],
     }),
@@ -32,7 +27,6 @@ app.use(
 app.use((req, res, next) => {
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Origin', req.headers.origin || corsOrigin);
-
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         return res.status(204).json({});
@@ -40,26 +34,18 @@ app.use((req, res, next) => {
     next();
 });
 
-// bodyParser와 express.json()은 CORS 설정 바로 다음
 app.use(bodyParser.json());
 app.use(express.json());
 
-// 로깅 및 쿠키 파서 미들웨어
 app.use(LogMiddleware);
-//app.use(TestLogMiddleware);
 app.use(cookieParser());
 
-// Passport 초기화 및 라우팅 설정
-// app.use(passport.initialize()); // Passport를 사용하는 경우 초기화 필요
 app.use('/', router);
 
-// 스웨거 설정
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
-// 에러 핸들링 미들웨어는 가장 마지막에 위치
 app.use(generalErrorHandler);
 
-// 금지어 목록 로드
 loadBannedWords()
     .then(() => {
         console.log('금지어 목록이 메모리에 로드되었습니다.');
