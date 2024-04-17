d057026f8d4c8fbc223c2d9ed52f771c5f3ef336
LetyPark <sunghee.leticia@gmail.com>
2024-04-17 17:13:00 +0900

    Refactor : worry & comment 에러 미들웨어에서 처리하기

--- src/routes/comments/comment.controller.js
+++ src/routes/comments/comment.controller.js
@@ -1,42 +1,38 @@
 import * as commentService from './comment.service.js';
 
 // # 로그인한 유저에게 온 전체 메세지 조회 (매칭된 첫고민 or 이후 답장)
-export const getAllLatestMessagesController = async (req, res) => {
+export const getAllLatestMessagesController = async (req, res, next) => {
     try {
         const userId = res.locals.user.userId;
         // const { userId } = req.body;
         const latestMessages = await commentService.getAllLatestMessages(+userId);
         if (latestMessages.length === 0) {
-            return res.status(204).json({ error: '아직 메세지가 도착하지 않았습니다' });
+            const error = new Error('아직 메세지가 도착하지 않았습니다');
+            error.status = 204;
+            throw error;
         }
-
         return res.json(latestMessages);
     } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: error.message });
+        next(error);
     }
 };
 
 // # 답장 상세조회
-export const getCommentDetailController = async (req, res) => {
+export const getCommentDetailController = async (req, res, next) => {
     try {
         const { commentId } = req.params;
         const userId = res.locals.user.userId;
         // const { userId } = req.body;
 
         if (!commentId) {
-            return res.status(400).json({ error: '데이터 형식이 일치하지 않습니다' });
+            const error = new Error('데이터 형식이 일치하지 않습니다');
+            error.status = 400;
+            throw error;
         }
-
         const details = await commentService.getCommentDetail(+commentId, +userId);
         res.json(details);
     } catch (error) {
-        if (error.message === '해당하는 답장이 존재하지 않습니다') {
-            return res.status(404).json({ error: error.message });
-        } else if (error.message === '답장을 조회할 권한이 없습니다.') {
-            return res.status(403).json({ error: error.message });
-        }
-        res.status(500).json({ error: error.message });
+        next(error);
     }
 };
 
@@ -49,43 +45,15 @@ export const createReplyController = async (req, res, next) => {
         // const { userId, content, fontColor } = req.body;
 
         if (!worryId || !content || !fontColor) {
-            return res.status(400).json({ error: '데이터 형식이 올바르지 않습니다' });
+            const error = new Error('데이터 형식이 일치하지 않습니다');
+            error.status = 400;
+            throw error;
         }
 
         const comment = await commentService.createReply(+worryId, +commentId, content, +userId, fontColor);
 
         return res.status(201).json({ message: '답변이 전송되었습니다.', comment });
     } catch (error) {
-        switch (error.message) {
-            case '금지어가 포함된 내용은 등록할 수 없습니다.':
-                return res.status(400).json({ error: error.message });
-            case '답변 작성 권한이 없습니다.':
-                return res.status(403).json({ error: error.message });
-            case '이미 답변을 작성했습니다.':
-                return res.status(409).json({ error: error.message });
-            default:
-                return res.status(500).json({ error: '서버 오류가 발생했습니다.' });
-        }
+        next(error);
     }
 };
-
-// // # 불쾌한 답장 신고하기
-// export const reportCommentController = async (req, res, next) => {
-//     const { commentId } = req.params;
-//     const userId = res.locals.user.userId;
-//     const { reportReason } = req.body;
-
-//     try {
-//         await commentService.reportComment(+commentId, +userId, reportReason);
-//         res.status(200).json({ message: '답변이 성공적으로 신고되었습니다.' });
-//     } catch (error) {
-//         if (error.message === '해당하는 답장이 존재하지 않습니다') {
-//             return res.status(404).json({ error: error.message });
-//         } else if (error.message === '답장을 신고할 권한이 없습니다.') {
-//             return res.status(403).json({ error: error.message });
-//         } else if (error.message === '해당 답장은 이미 신고되었습니다') {
-//             return res.status(409).json({ error: error.message });
-//         }
-//         res.status(500).json({ error: error.message });
-//     }
-// };
--- src/routes/worries/worry.router.js
+++ src/routes/worries/worry.router.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import {
     createWorryController,
-    WorryDetailController,
+    worryDetailController,
     deleteOldMessagesController,
     deleteSelectedWorryController,
     reportMessageController,
@@ -19,7 +19,7 @@ router.post('/', authMiddleware, createWorryController);
 // router.post('/', createWorryController);
 
 // 고민메세지 상세조회
-router.get('/:worryId', authMiddleware, WorryDetailController);
+router.get('/:worryId', authMiddleware, worryDetailController);
 // router.get('/:worryId', WorryDetailController);
 
 // 최초 고민에 24시간 동안 답변없는 고민 or 첫답장은 있지만 이후 답장이 24시간 동안 없는 고민 삭제
@@ -28,7 +28,7 @@ router.delete('/', deleteOldMessagesController);
 // 곤란한 메세지 선택 삭제
 router.delete('/:worryId/:commentId?', authMiddleware, deleteSelectedWorryController);
 
-// 불쾌한 고민 신고하기
+// 불쾌한 메세지 신고하기
 router.post('/:worryId/comments/:commentId?/report', authMiddleware, reportMessageController);
 
 export default router;
