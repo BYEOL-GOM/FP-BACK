607cc2424c247edfd3107e84fdab7a266905ddc8
rosa1435 <158241447+rosa1435@users.noreply.github.com>
2024-04-17 22:00:31 +0900

    FIX: cors설정 수정

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ const PORT = 3000; // 환경 변수에서 포트를 설정할 수 있도록 변�
 // CORS 미들웨어 설정
 app.use(
     cors({
-        origin: 'http://star-bear.s3-website.eu-north-1.amazonaws.com/',
+        origin: '*',
         methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
         allowedHeaders: ['Content-Type', 'Authorization'],
     }),
