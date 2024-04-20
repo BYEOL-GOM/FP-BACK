503dc4c26e68731d8fa213afa2ecbd51680d591f
LetyPark <sunghee.leticia@gmail.com>
2024-04-20 19:10:18 +0900

    app.js -cors 수정

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', req.headers.origin); // 요청이 온 원점(origin)을 허용
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        res.status(204).json({}); // 상태 코드 204와 함께 빈 응답을 보냄
+        return res.status(204).json({});
     }
     next();
 });
