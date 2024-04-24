abd6024168895849aad500b9c34aed329262b473
LetyPark <sunghee.leticia@gmail.com>
2024-04-24 16:49:43 +0900

    Refactor : sentry 추가

--- src/app.js
+++ src/app.js
@@ -11,10 +11,21 @@ import { loadBannedWords } from './utils/bannedWordsLoader.js';
 import { swaggerUi, specs } from './swagger/swaggerOptions.js';
 import './scheduler.js';
 import validUrl from 'valid-url';
+import * as Sentry from '@sentry/node';
+import { Integrations } from '@sentry/tracing';
 
 const app = express();
 
-// 환경 변수에서 CONTAINER_PORT를 불러옵니다. 없다면 기본값으로 3000을 사용합니다.
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [new Integrations.Http({ tracing: true }), new Sentry.Integrations.Express({ app })],
+    tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler()); // Sentry 요청 핸들러
+app.use(Sentry.Handlers.tracingHandler()); // Sentry 트레이싱 핸들러
+
+// 환경 변수에서 CONTAINER_PORT를 불러옵니다. 없다면 기본값으로 3000을 사용
 const PORT = process.env.CONTAINER_PORT || 3000;
 
 // CORS_ORIGIN 환경 변수가 유효한 URL 형식인지 검증
@@ -59,6 +70,7 @@ app.get('/health', (req, res) => {
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
+app.use(Sentry.Handlers.errorHandler());
 app.use(generalErrorHandler);
 
 loadBannedWords()
@@ -68,6 +80,11 @@ loadBannedWords()
     .catch((error) => {
         console.error('금지어 목록 로딩 중 오류 발생:', error);
     });
+
+// sentry 확인을 위해 의도적으로 에러 발생시키기
+app.get('/debug-sentry', function mainHandler(req, res) {
+    throw new Error('My first Sentry error!');
+});
 
 app.listen(PORT, () => {
     console.log(`${PORT} 포트로 서버가 열렸어요!`);
