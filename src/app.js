7acfc545a319e062c032c0c8e18bc7e82d993695
LetyPark <sunghee.leticia@gmail.com>
2024-04-26 18:39:05 +0900

    Refactor : 빠진 중괄호 추가

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ if (process.env.SENTRY_DSN) {
     // Sentry 요청 및 트레이싱 핸들러를 사용
     app.use(Sentry.Handlers.requestHandler());
     app.use(Sentry.Handlers.tracingHandler());
+}
 
 // // CORS_ORIGIN 환경 변수가 유효한 URL 형식인지 검증
 const corsOrigin = process.env.CORS_ORIGIN;
@@ -66,7 +67,6 @@ app.use(
 
 app.use(bodyParser.json());
 app.use(express.json());
-
 
 app.use(LogMiddleware);
 app.use(cookieParser());
