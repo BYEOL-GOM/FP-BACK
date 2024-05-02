7d8b40b0e9dd3435fe0e670856c94c9adb5cf93a
LetyPark <sunghee.leticia@gmail.com>
2024-05-02 13:32:52 +0900

    테스트

--- src/app.js
+++ src/app.js
@@ -94,11 +94,9 @@ app.get('/debug-sentry', function mainHandler(req, res) {
 });
 
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'test' });
+    res.status(200).json({ message: 'test OK' });
 });
-app.get('/just', (req, res) => {
-    res.status(200).json({ message: 'flowers' });
-});
+
 app.listen(PORT, () => {
     console.log(`${PORT} 포트로 서버가 열렸어요!`);
 });
